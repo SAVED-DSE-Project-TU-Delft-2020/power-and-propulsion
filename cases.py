6fcd348c02ec32407ed55efa418fe755b9c7830d
Raven van Ewijk <raven.ewijk@klm.com>
2020-05-08 10:04:19 +0200

    Added Case 4

--- cases.py
+++ cases.py
@@ -8,12 +8,21 @@ Created on Thu May  7 12:19:59 2020
 import matplotlib.pyplot as plt
 import scipy as np
 
-
+cd0      = 0.0184
+V        = 28
+k1       = 0.0245
+k2       = 0
+g0       = 9.81
+WS_range = np.arange(5,200,1)
+q        = 457.57
 
 #___CASE 1 CONSTANT ALITITUDE/SPEED CRUISE
 
 def Case1(q, cd0, alpha, WS_range):
-    
+    '''
+    calculates the wing loading for constant altitude and speed cruise
+    inputs are q, cd0, alpha, ws_range
+    '''
     T_W = []
     W_S = []
     
@@ -28,7 +37,10 @@ def Case1(q, cd0, alpha, WS_range):
 #___CASE 2 CONSTANT SPEED CLIMB
         
 def Case2(cd0, k1, k2, V, dhdt, q, alpha, beta, WS_range):
-    
+    '''
+    calculates the wing loading for constant speed climb
+    inputs are cd0, k1, k2, cruise V, climb rate dhdt, q, alpha and beta
+    '''
     T_W = []
     W_S = []
     
@@ -43,7 +55,10 @@ def Case2(cd0, k1, k2, V, dhdt, q, alpha, beta, WS_range):
 #___CASE 3 CONSTANT ALTITUDE/SPEED TURN
     
 def Case3(cd0, k1, k2, V, g0, Rc, q, alpha, beta, WS_range):
-    
+    '''
+    calculates the wing loading for constant altitude/ speed turn
+    inputs are cd0, k1, k2, cruise V, gravity g0, turn radius Rc, q, alpha and beta
+    '''
     T_W = []
     W_S = []
     
@@ -59,11 +74,17 @@ def Case3(cd0, k1, k2, V, g0, Rc, q, alpha, beta, WS_range):
     
 #___CASE 4 HORIZONTAL ACCELERATION
 
-def Case4():
+def Case4(cd0, k1, k2, dVdt, g0, q, alpha, beta, WS_range):
     
-    return
+    T_W = []
+    W_S = []
+    
+    for WS in WS_range:
+        TW = beta / alpha * (k1 * beta / q * WS + k2 + cd0 / beta * q * WS + 1 / g0 * dVdt) 
+        T_W.append(TW)
+        W_S.append(WS)
 
-
+    plt.plot(W_S, T_W)
 
 #___CASE 8 SERVICE CEILING
   
@@ -78,6 +99,14 @@ def Case8(cd0, k1, k2, V, dhdt, CL, q, alpha, beta, WS_range):
         W_S.append(WS)
 
     plt.plot(W_S, T_W)
+    
+
+#___CASE 0 STALL SPEED
+    
+def Case0(rho, Vs, CLmax):
+    
+    WS = 1/2 * rho * Vs**2 * CLmax
+    plt.axvline(x=WS)
 
 
 
