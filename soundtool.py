8e8f9306f008f2630341c76f614ad0384728fa75
ravenvanewijk <ravenvanewijk1@gmail.com>
2020-06-04 12:29:19 +0200

    Fixed numerous bugs. Added heat map plotting. Added function descriptions

--- soundtool.py
+++ soundtool.py
@@ -10,20 +10,37 @@ from scipy import special
 from isacalc import isa
 import matplotlib.pyplot as plt
 
-#ORDERED ROTATIONAL NOISE
+
+#POSITIONS
+spanloc_out = 0.7/2
+spanloc_in = 0.35/2
+b = 3 
+
+engine_right_out = [0, spanloc_out*3, 0]
+engine_right_in = [0, spanloc_in*3, 0]
+engine_left_out = [0, -spanloc_out*3, 0]
+engine_left_in = [0, -spanloc_in*3, 0]
+xyzsource = np.array((engine_right_out, engine_right_in, engine_left_out, engine_left_in))
+FW_skeleton = [[],[]]
+
+
 
 def p_m(m, S, R, P_h, T, B, M_t, theta):
-    
+    '''
+    Calculates the rotational noise at a certain position (thrust/ power noise)
+    '''
+    theta = theta+0.5*np.pi
     A = R**2*np.pi
     J_mb = np.special.jn(m*B, 0.8*M_t*m*B*np.sin(theta))
     p = 169.3*m*B*R*M_t/(S*A)*(0.76*P_h/M_t**2-T*np.cos(theta))*J_mb
     
-    return p
+    return np.absolute(p)
 
-#VORTEX NOISE
     
 def SPL_vortex(A_b, V_07):
-    
+    ''' 
+    Calculates the vortex noise at 300 ft
+    '''
     k = 3.8*10**(-27)
     SPL = 10*np.log10(k*A_b*V_07**6/(10**-16))
     
@@ -32,21 +49,27 @@ def SPL_vortex(A_b, V_07):
 #NOISE CONVERSIONS
     
 def p_to_SPL(p):
-    
+    '''
+    Converts a pressure level to SPL
+    '''
     p_ref = 0.0002 #dynes/cm^2
     SPL = 20*np.log10(p/p_ref)
     
     return SPL
 
 def SPL_to_p(SPL):
-    
+    '''
+    Converts SPL to a pressure level
+    '''
     p_ref = 0.0002 #dynes/cm^2
     p = 10**(SPL/20)*p_ref
     
     return p
 
 def SPL_to_PWL_dist(SPL, S, S_obs=0):
-    
+    '''
+    Converts SPL to other distances, or to a power level
+    '''
     S = S*0.3048
     A = 4*np.pi*S**2
     if S_obs==0:
@@ -63,14 +86,18 @@ def SPL_to_PWL_dist(SPL, S, S_obs=0):
 #UNIT CONVERSIONS
 
 def A_ft(d_inch):
-    
+    '''
+    Calculates circle area in ft based on diameter in inch
+    '''
     d_ft = 0.0833333333* d_inch
     A_ft = d_ft**2*np.pi/4
     
     return A_ft
 
 def V_r(RPM, d_inch, r):
-    
+    '''
+    Calculates velocity based on radial position and rpm
+    '''
     rads = 0.104719755*RPM
     r_inch = d_inch/2
     r_ft = 0.0833333333*r_inch
@@ -79,26 +106,18 @@ def V_r(RPM, d_inch, r):
     
     return V_r
 
-#POSITION VECTORS
-spanloc_out = 0.7/2
-spanloc_in = 0.35/2
-b = 3 
-
-engine_right_out = [0, spanloc_out*3, 0]
-engine_right_in = [0, spanloc_in*3, 0]
-engine_left_out = [0, -spanloc_out*3, 0]
-engine_left_in = [0, -spanloc_in*3, 0]
-xyzsource = np.array((engine_right_out, engine_right_in, engine_left_out, engine_left_in))
-
 
 def position(xyzsource, xyzobserver):
+    '''
+    Calculates the norm and angle between a set of points
+    '''
     S = np.array([])
     thetas = np.array([])
     for xyz in xyzsource:
         distance = np.linalg.norm(np.array(xyz)-np.array(xyzobserver))
         opposite = np.absolute(xyz[0]-xyzobserver[0])
         adjacent = np.absolute(xyz[1]-xyzobserver[1])
-        angle = np.arctan2(opposite, adjacent)+0.5*np.pi
+        angle = np.arctan(opposite/ adjacent)
         S = np.append(S,distance/0.3048)
         thetas = np.append(thetas, angle)
     
@@ -107,7 +126,9 @@ def position(xyzsource, xyzobserver):
 #NOISE CALCULATION
 
 def noise(xyzsource, d_inch, P_h, T, B, RPM, h):
-    
+    '''
+    Calculates the SPL on a set of points
+    '''
     R_ft = 0.0833333333*d_inch/2
     m = 1
     gamma, R, Temp = 1.4, 287, isa(h)[1]
@@ -117,37 +138,39 @@ def noise(xyzsource, d_inch, P_h, T, B, RPM, h):
     #Assumption blade area
     A_b = 2/B*1/9
     
-    p_rn = list()
-    p_v = list()
     SPLlst = list()
-    xlst = list()
-    ylst = list()
-    
-    for y in np.arange(-10,0.1,1):
-        for x in np.arange(-10,0.1,1): 
+    xrange = np.arange(-0.6,0.5+0.002,0.002)
+    yrange = np.arange(-1.6,1.6+0.002,0.002)
+    for x in xrange:
+        for y in yrange: 
             xyzobserver=[x,y,0]
-
             S, theta = position(xyzsource, xyzobserver)
             
+            p_rn = list()
+            p_v = list()
             for S, theta in zip(S, theta):
                 #ORDERED ROTATIONAL NOISE PRESSURE AT OBSERVER
                 rotationp = p_m(m, S, R_ft, P_h, T, B, M_t, theta)
                 p_rn.append(rotationp)
-                
                 #VORTEX NOISE AT OBSERVER
                 vortexSPL = SPL_vortex(A_b, V_07)
                 vortexSPL_obs = SPL_to_PWL_dist(vortexSPL, 300, S_obs=S)
                 vortexp = SPL_to_p(vortexSPL_obs)
                 p_v.append(vortexp)
 
-        
+
             p_sum = sum(p_rn+p_v)
             SPL = p_to_SPL(p_sum)
-            SPLlst.append(SPL)
-            xlst.append(x)
-            ylst.append(y)
+            if SPL>130:
+                SPLlst.append(np.nan)
+            else:
+                SPLlst.append(SPL)
 
-    return np.array(SPLlst).reshape(int(np.sqrt(len(SPLlst))),int(np.sqrt(len(SPLlst))))
+    SPLlst = np.array(SPLlst).reshape(len(xrange),len(yrange))
+    window = [yrange[0],yrange[-1],xrange[-1],xrange[0]]
+    plt.imshow(SPLlst, cmap='Reds',extent=window)
+    plt.colorbar()
+    return SPLlst
 
 
 
@@ -156,13 +179,13 @@ def noise(xyzsource, d_inch, P_h, T, B, RPM, h):
 #for theta in np.arange(0,2*np.pi*1.026,0.05):
 #    p=p_m(1,3,0.645833333075,1.36,12.521858094085683,3, 0.75, theta)
 #    if p>0:
-#        thetalst.append(theta-0.5*np.pi)
+#        thetalst.append(theta)
 #        plst.append(p)
 #    else:
-#        thetalst.append(theta-0.5*np.pi)
+#        thetalst.append(theta)
 #        plst.append(-p)
 #
-##plt.plot(thetalst,plst)
+#plt.plot(thetalst,plst)
 #plt.polar(thetalst,plst)
 #plt.show()
 #plt.ylabel('p')
