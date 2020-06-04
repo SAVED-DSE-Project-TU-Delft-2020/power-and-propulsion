901a9f051fa82bf18458d26cf6487890025d2477
ravenvanewijk <ravenvanewijk1@gmail.com>
2020-06-04 16:36:02 +0200

    added requirement function, fixed thrust bug

--- soundtool.py
+++ soundtool.py
@@ -22,13 +22,14 @@ engine_left_out = [0.4613491583980512, -spanloc_out*3, 0]
 engine_left_in = [0.2306745791990256, -spanloc_in*3, 0]
 xyzsource = np.array((engine_right_out, engine_right_in, engine_left_out, engine_left_in))
 FW_skeleton = [[0,0.6590702262829303,0.9027022644938187,0.6960915377453957,0.9027022644938187,0.6590702262829303,0],[0,-1.5,-1.5,0,1.5,1.5,0]]
-
+CG_loc = [-1,0]
 
 
 def p_m(m, S, R, P_h, T, B, M_t, theta):
     '''
     Calculates the rotational noise at a certain position (thrust/ power noise)
     '''
+    T = 0.22480894244319*T
     theta = theta+0.5*np.pi
     A = R**2*np.pi
     J_mb = np.special.jn(m*B, 0.8*M_t*m*B*np.sin(theta))
@@ -68,7 +69,7 @@ def SPL_to_p(SPL):
 
 def SPL_to_PWL_dist(SPL, S, S_obs=0):
     '''
-    Converts SPL to other distances, or to a power level
+    Converts SPL to other distances, or to a power level. S in ft
     '''
     S = S*0.3048
     A = 4*np.pi*S**2
@@ -82,7 +83,15 @@ def SPL_to_PWL_dist(SPL, S, S_obs=0):
         SPL_new = SPL+10*np.log10(ratio)
         return SPL_new
 
-
+def PWL_to_SPL(PWL, S):
+    '''
+    Calculates noise pressure based on noise power source. S in m
+    '''
+    A = 4*np.pi*S**2
+    SPL = PWL-10*np.log10(A)
+    
+    return SPL
+    
 #UNIT CONVERSIONS
 
 def A_ft(d_inch):
@@ -125,7 +134,7 @@ def position(xyzsource, xyzobserver):
 
 #NOISE CALCULATION
 
-def noise(xyzsource, d_inch, P_h, T, B, RPM, h):
+def noise_heat(xyzsource, d_inch, P_h, T, B, RPM, h):
     '''
     Calculates the SPL on a set of points.
     Also produces heat map of the surroundings
@@ -137,7 +146,7 @@ def noise(xyzsource, d_inch, P_h, T, B, RPM, h):
     V_07 = V_r(RPM, d_inch, 0.7) 
     
     #Assumption blade area
-    A_b = 2/B*1/9
+    A_b = 2/B*1/12
     
     SPLlst = list()
     xrange = np.arange(-0.6,1+0.01,0.01)
@@ -160,9 +169,9 @@ def noise(xyzsource, d_inch, P_h, T, B, RPM, h):
                 p_v.append(vortexp)
 
 
-            p_sum = sum(p_rn+p_v)
+            p_sum = sum(p_rn)+sum(p_v)
             SPL = p_to_SPL(p_sum)
-            if SPL>130:
+            if SPL>120:
                 SPLlst.append(np.nan)
             else:
                 SPLlst.append(SPL)
@@ -174,6 +183,51 @@ def noise(xyzsource, d_inch, P_h, T, B, RPM, h):
     plt.plot(FW_skeleton[1],FW_skeleton[0],"black")
     return SPLlst
 
+
+def req_noise(xyzsource, d_inch, P_h, T, B, RPM, h, r, CG, PWL_req):
+    
+    SPL_req = PWL_to_SPL(PWL_req,r)
+    print(SPL_req)
+    SPLlst = list()
+    
+    R_ft = 0.0833333333*d_inch/2
+    m = 1
+    gamma, R, Temp = 1.4, 287, isa(h)[1]
+    M_t = V_r(RPM, d_inch, 1)*0.3048/np.sqrt(gamma*R*Temp)
+    V_07 = V_r(RPM, d_inch, 0.7) 
+    
+    #Assumption blade area
+    A_b = 2/B*1/12
+    
+    for theta in np.arange(0,360+1,1):
+        
+        x = r*np.sin(theta)
+        y = r*np.cos(theta)
+        
+        xyzobserver=[x,y,0]
+        S, theta = position(xyzsource, xyzobserver)
+    
+        p_rn = list()
+        p_v = list()
+        
+        for S, theta in zip(S, theta):
+            #ORDERED ROTATIONAL NOISE PRESSURE AT OBSERVER
+            rotationp = p_m(m, S, R_ft, P_h, T, B, M_t, theta)
+            p_rn.append(rotationp)
+            #VORTEX NOISE AT OBSERVER
+            vortexSPL = SPL_vortex(A_b, V_07)
+            vortexSPL_obs = SPL_to_PWL_dist(vortexSPL, 300, S_obs=S)
+            vortexp = SPL_to_p(vortexSPL_obs)
+            p_v.append(vortexp)
+        
+        p_sum = sum(p_rn+p_v)
+        
+        SPL = p_to_SPL(p_sum)
+        SPLlst.append(SPL)
+        
+    return max(SPLlst)
+    
+    
 
 #BUTTERFLY PLOT
 #plst=list()
